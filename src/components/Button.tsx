36f30afd10b5b7c7ed74cc657216a46dabb29642
akane9506 <akane19950611@gmail.com>
2025-07-16 22:27:53 -0700

    login page updated

---
+++ src/components/Button.tsx
@@ -0,0 +1,3 @@
+import { Button } from "@heroui/react";
+
+export default Button;
---
+++ src/pages/login/LoginPage.tsx
@@ -0,0 +1,12 @@
+import LoginForm from "./Form";
+
+const LoginPage = () => {
+  return (
+    <div className="login">
+      <h1 className="text-2xl font-medium my-6">星愿小屋管理系统</h1>
+      <LoginForm />
+    </div>
+  );
+};
+
+export default LoginPage;
