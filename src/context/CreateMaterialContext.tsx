b9b8b606cb45e66422afefc2471b3a5e8327304c
akane9506 <akane19950611@gmail.com>
2025-08-11 23:25:12 -0700

    upload image context updated

---
+++ src/context/CreateMaterialContext.tsx
@@ -0,0 +1,73 @@
+import React from "react";
+
+type CreateStep = "idle" | "uploaded" | "processed" | "ready";
+type CreateMaterialStore = {
+  currStep: CreateStep;
+  uploadedImageUrls: string[];
+  processedImageUrls: string[];
+};
+
+interface CreateMaterialProps {
+  currStep: CreateStep;
+  uploadedImageUrls: string[];
+  processedImageUrls: string[];
+  setCurrStep: (currStep: CreateStep) => void;
+  setUploadImageUrls: (urls: string[]) => void;
+  setProcessedImageUrls: (urls: string[]) => void;
+  clearContext: () => void; // use this to clear the context when upload completed
+}
+
+const CreateMaterialContext = React.createContext<CreateMaterialProps>({
+  currStep: "idle",
+  uploadedImageUrls: [],
+  processedImageUrls: [],
+  setCurrStep: () => {},
+  setProcessedImageUrls: () => {},
+  setUploadImageUrls: () => {},
+  clearContext: () => {},
+});
+
+const initialState: CreateMaterialStore = {
+  currStep: "idle",
+  uploadedImageUrls: [],
+  processedImageUrls: [],
+};
+
+const CreateMaterialProvider = ({ children }: { children: React.ReactNode }) => {
+  const [createMaterialStore, setCreateMaterialStore] =
+    React.useState<CreateMaterialStore>(initialState);
+
+  const handleSetCurrStep = (currStep: CreateStep) => {
+    setCreateMaterialStore((prev) => ({ ...prev, currStep }));
+  };
+
+  const handleSetUploadedImageUrls = (urls: string[]) => {
+    setCreateMaterialStore((prev) => ({ ...prev, uploadedImageUrls: urls }));
+  };
+
+  const handleSetProcessedImageUrls = (urls: string[]) => {
+    setCreateMaterialStore((prev) => ({ ...prev, processedImageUrls: urls }));
+  };
+
+  const handleClearContext = () => {
+    setCreateMaterialStore(initialState);
+  };
+
+  return (
+    <CreateMaterialContext.Provider
+      value={{
+        currStep: createMaterialStore.currStep,
+        uploadedImageUrls: createMaterialStore.uploadedImageUrls,
+        processedImageUrls: createMaterialStore.processedImageUrls,
+        setCurrStep: handleSetCurrStep,
+        setUploadImageUrls: handleSetUploadedImageUrls,
+        setProcessedImageUrls: handleSetProcessedImageUrls,
+        clearContext: handleClearContext,
+      }}
+    >
+      {children}
+    </CreateMaterialContext.Provider>
+  );
+};
+
+export default CreateMaterialProvider;
--- src/pages/dashboard-materials/DashboardMaterials.tsx
+++ src/pages/dashboard-materials/DashboardMaterials.tsx
@@ -1,71 +1,11 @@
-import { Tabs, Tab, cn } from "@heroui/react";
-import UploadFile from "./UploadFile";
-import Alert from "@/components/Alert";
-import Button from "@components/Button";
-import React from "react";
-
-type TabType = "characters" | "decorations" | "background";
-
-type TabContent = {
-  id: TabType;
-  label: string;
-  content: string;
-};
+import CreateMaterialProvider from "@/context/CreateMaterialContext";
+import DashboardMaterialsBody from "./DashboardMaterialsBody";
 
 const DashboardMaterials = () => {
-  const tabs: TabContent[] = [
-    { id: "characters", label: "角色", content: "角色素材内容" },
-    { id: "decorations", label: "装饰", content: "装饰素材内容" },
-    { id: "background", label: "背景", content: "背景素材内容" },
-  ];
-
-  const [uploadMode, setUploadMode] = React.useState<boolean>(true);
-  const [currTab, setCurrtab] = React.useState<TabType>("characters");
-
-  const [errorMessage, setErrorMessage] = React.useState<string>("");
-  const handleSetErrorMessage = (errorMessage: string) => {
-    setErrorMessage(errorMessage);
-  };
-
   return (
-    <div className="dashboard-materials">
-      <div className="dashboard-materials__header relative">
-        <Tabs
-          items={tabs}
-          selectedKey={currTab}
-          onSelectionChange={(key) => setCurrtab(key as TabType)}
-          radius="full"
-          color="secondary"
-          variant="bordered"
-          className="col-start-2"
-        >
-          {(item) => {
-            return <Tab key={item.id} title={item.label} className="px-6" />;
-          }}
-        </Tabs>
-        <Button
-          variant={uploadMode ? "shadow" : "bordered"}
-          color="success"
-          className={cn(
-            "text-xl min-w-8 min-h-8 w-8 h-8",
-            uploadMode && "text-background rotate-45 transition-transform duration-200"
-          )}
-          radius="full"
-          size="sm"
-          onPress={() => {
-            setUploadMode((prev) => !prev);
-          }}
-        >
-          +
-        </Button>
-      </div>
-      <div className="flex-1 flex items-center justify-center text-secondary-200 w-full relative">
-        {errorMessage.length !== 0 && (
-          <Alert color="danger" variant="faded" title={errorMessage} />
-        )}
-        {uploadMode && <UploadFile onError={handleSetErrorMessage} />}
-      </div>
-    </div>
+    <CreateMaterialProvider>
+      <DashboardMaterialsBody />
+    </CreateMaterialProvider>
   );
 };
 
